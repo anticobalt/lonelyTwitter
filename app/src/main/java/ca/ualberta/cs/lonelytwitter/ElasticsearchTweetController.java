4abdb64909f33df353bdebb49d7d620545aee3ba
anticobalt <anticobalt@users.noreply.github.com>
2018-10-11 17:32:01 -0600

    Fix bundle

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/ElasticsearchTweetController.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/ElasticsearchTweetController.java
@@ -13,62 +13,17 @@ import io.searchbox.core.Index;
  * Created by romansky on 10/20/16.
  */
 public class ElasticsearchTweetController {
-    private static JestDroidClient client;
 
-    // TODO we need a function which adds tweets to elastic search
-    public static class AddTweetsTask extends AsyncTask<NormalTweet, Void, Void> {
+    JestDroidClient client = null;
 
-        @Override
-        protected Void doInBackground(NormalTweet... tweets) {
-            //verifySettings();
+    public static void addTweets(Tweet tweet){
+        setClient();
+    }
 
-            for (NormalTweet tweet : tweets) {
-                Index index = new Index.Builder(tweet).index("testing").type("tweet").build();
+    public static void setClient(){
+        if (client == null){
 
-                try {
-                    // where is the client?
-                }
-                catch (Exception e) {
-                    Log.i("Error", "The application failed to build and send the tweets");
-                }
-
-            }
-            return null;
         }
     }
 
-    // TODO we need a function which gets tweets from elastic search
-/*    public static class GetTweetsTask extends AsyncTask<String, Void, ArrayList<NormalTweet>> {
-        @Override
-        protected ArrayList<NormalTweet> doInBackground(String... search_parameters) {
-            verifySettings();
-
-            ArrayList<NormalTweet> tweets = new ArrayList<NormalTweet>();
-
-                // TODO Build the query
-
-            try {
-               // TODO get the results of the query
-            }
-            catch (Exception e) {
-                Log.i("Error", "Something went wrong when we tried to communicate with the elasticsearch server!");
-            }
-
-            return tweets;
-        }
-    }*/
-
-
-
-
-    public static void verifySettings() {
-        if (client == null) {
-            DroidClientConfig.Builder builder = new DroidClientConfig.Builder("http://cmput301.softwareprocess.es:8080");
-            DroidClientConfig config = builder.build();
-
-            JestClientFactory factory = new JestClientFactory();
-            factory.setDroidClientConfig(config);
-            client = (JestDroidClient) factory.getObject();
-        }
-    }
 }
